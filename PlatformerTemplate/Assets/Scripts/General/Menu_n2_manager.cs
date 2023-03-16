4dc97762fc6c59ebe0fab085bb48c861354fdda2
Sacha <107134875+xivsacha@users.noreply.github.com>
2023-03-16 16:32:20 +0100

    Commit des menus

--- PlatformerTemplate/Assets/Scripts/General/Menu_n2_manager.cs
+++ PlatformerTemplate/Assets/Scripts/General/Menu_n2_manager.cs
@@ -8,12 +8,12 @@ public class Menu_n2_manager : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
 
     public void Next_Level()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
 
     public void Prev_Level()
--- PlatformerTemplate/Assets/Scripts/General/Menu_n3_manager.cs
+++ PlatformerTemplate/Assets/Scripts/General/Menu_n3_manager.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu_n1_manager : MonoBehaviour
+public class Menu_n3_manager : MonoBehaviour
 {
 
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(3);
     }
 
     public void Next_Level()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(4);
     }
 
     public void Prev_Level()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(2);
     }
 }
